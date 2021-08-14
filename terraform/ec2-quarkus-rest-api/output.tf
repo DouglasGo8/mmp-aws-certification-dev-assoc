146ddde03914f52779e39d4261f2ca10e5856992
DouglasGo8 <abraham.dias.douglas@gmail.com>
2021-08-14 05:28:14 -0300

    EC2 in progress

--- terraform/ec2-quarkus-rest-api/output.tf
+++
@@ -1,3 +0,0 @@
-output "instance" {
-  value = "${aws_instance.t2-nano-inst.public_ip}"
-}
--- terraform/ec2-quarkus-rest-api/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "aws" {
-  region  = var.AWS_REGION  # The region in which you want to Operate
-  profile = var.AWS_PROFILE # The profile in which you want to Operate
-}
