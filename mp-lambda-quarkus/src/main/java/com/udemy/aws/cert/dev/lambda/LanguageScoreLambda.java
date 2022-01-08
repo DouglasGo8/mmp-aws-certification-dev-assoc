cd4b6dc6e42f2e2fd808c37ed6541eaf2c0947c1
DouglasGo8 <abraham.dias.douglas@gmail.com>
2022-01-08 15:42:06 -0300

    Doc Api

--- mp-lambda-quarkus/src/main/java/com/udemy/aws/cert/dev/lambda/LanguageScoreLambda.java
+++ mp-lambda-quarkus/src/main/java/com/udemy/aws/cert/dev/lambda/LanguageScoreLambda.java
@@ -2,8 +2,8 @@ package com.udemy.aws.cert.dev.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import com.udemy.aws.cert.dev.lambda.domain.Language;
 import com.udemy.aws.cert.dev.lambda.dto.LanguageScoreDto;
 
 import javax.inject.Named;
@@ -13,22 +13,21 @@ import java.util.HashMap;
  * @author dougdb
  */
 @Named("languageScoreLambda")
-public class LanguageScoreLambda implements RequestHandler<Language, APIGatewayProxyResponseEvent> {
+public class LanguageScoreLambda implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
 
   //@Inject
   //ProducerTemplate producerTemplate;
 
   @Override
-  public APIGatewayProxyResponseEvent handleRequest(Language input, Context context) {
+  public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
 
     var logger = context.getLogger();
 
     logger.log("handleRequest the Main Function");
     logger.log(context.toString());
 
-
-    var dto = new LanguageScoreDto("5", input.getLanguage()).toString();
+    var dto = new LanguageScoreDto("5", "Java").toString();
 
     //this.producerTemplate.requestBody("{{language.score.direct.endpoint}}",
     //input, String.class);
--- terraform/lambda/api-gateway.tf
+++ terraform/lambda/api-gateway.tf
@@ -1,17 +1,20 @@
 resource "aws_api_gateway_rest_api" "hello_lambda_rest_api_gwt" {
   name = "hello_lambda_rest_api"
+  endpoint_configuration {
+    types = ["REGIONAL"] // EDGE, PRIVATE, REGIONAL
+  }
 }
 
 resource "aws_api_gateway_resource" "hello_lambda_api_gwt_resource" {
   rest_api_id = aws_api_gateway_rest_api.hello_lambda_rest_api_gwt.id
   parent_id   = aws_api_gateway_rest_api.hello_lambda_rest_api_gwt.root_resource_id
-  path_part   = "{proxy+}"
+  path_part   = "/{proxy+}"
 }
 
 resource "aws_api_gateway_method" "hello_lambda_api_gwt_method" {
   rest_api_id   = aws_api_gateway_rest_api.hello_lambda_rest_api_gwt.id
   resource_id   = aws_api_gateway_resource.hello_lambda_api_gwt_resource.id
-  http_method   = "ANY"
+  http_method   = "GET"
   authorization = "NONE"
 }
 
